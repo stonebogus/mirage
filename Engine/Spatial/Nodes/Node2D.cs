d89907eaa5f04e129b3c95a0b1a61af7e393b35f
Max <max@stonebogus.com>
2026-09-18 23:08:37 -0600

    faet: create Node2D & Node3D classes

---
+++ Engine/Spatial/Nodes/Node2D.cs
@@ -0,0 +1,40 @@
+using Mirage.Common.Events;
+using Mirage.Graph;
+using Mirage.Math.Vectors;
+
+namespace Mirage.Spatial.Nodes;
+
+/// <summary>
+/// Represents a node positioned in two-dimensional space.
+/// </summary>
+/// <param name="name">The initial name of the node.</param>
+/// <param name="options">
+/// The optional values used to initialize the node.
+/// </param>
+/// <remarks>
+/// Position, rotation, and scale are relative to the node's spatial parent.
+/// Rotation is expressed in radians.
+/// </remarks>
+public class Node2D(string name, NodeOptions? options = null) : Node(name, options)
+{
+    /// <summary>
+    /// Gets the local position of the node.
+    /// </summary>
+    public Store<Vector2D> Position { get; } = new(new Vector2D(0, 0));
+
+    /// <summary>
+    /// Gets the local rotation of the node, expressed in radians.
+    /// </summary>
+    /// <remarks>
+    /// Positive values represent counterclockwise rotation.
+    /// </remarks>
+    public Store<double> Rotation { get; } = new(0);
+
+    /// <summary>
+    /// Gets the local scale of the node.
+    /// </summary>
+    /// <remarks>
+    /// A value of <c>(1, 1)</c> represents the original scale.
+    /// </remarks>
+    public Store<Vector2D> Scale { get; } = new(new Vector2D(1, 1));
+}
