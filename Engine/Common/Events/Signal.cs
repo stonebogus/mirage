80e6b03639ba591d38431a5e5b030ae7da1b7195
Max <max@stonebogus.com>
2026-09-19 18:14:45 -0600

    refactor: remove redundant ThrowIfDestroyed call

--- Engine/Common/Events/Signal.cs
+++ Engine/Common/Events/Signal.cs
@@ -31,8 +31,6 @@ public class Signal<TPayload> : Event<TPayload>
     /// </exception>
     public void Fire(TPayload payload)
     {
-        ThrowIfDestroyed();
-
         Dispatch(payload);
     }
 }
