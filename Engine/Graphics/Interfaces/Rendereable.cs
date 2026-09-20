18a41548863dea8fd64804487166c832c4580f6a
Max <max@stonebogus.com>
2026-09-19 21:15:05 -0600

    feat: create Rendereable interface

---
+++ Engine/Graphics/Interfaces/Rendereable.cs
@@ -0,0 +1,17 @@
+namespace Mirage.Graphics.Interfaces;
+
+/// <summary>
+/// Defines an object capable of producing graphical content for rendering.
+/// </summary>
+public interface IRenderable
+{
+    /// <summary>
+    /// Creates the rendering context that describes how this object should
+    /// be rendered.
+    /// </summary>
+    /// <returns>
+    /// A rendering context containing the graphical data required to render
+    /// this object.
+    /// </returns>
+    RenderContext Render();
+}
