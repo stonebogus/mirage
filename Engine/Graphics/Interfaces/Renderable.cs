48c92ec27688fc99bc969b2f2dfca360ffa1dd68
Max <max@stonebogus.com>
2026-09-22 23:06:29 -0600

    refactor: use RenderData & RenderContext on IRenderable

---
+++ Engine/Graphics/Interfaces/Renderable.cs
@@ -0,0 +1,18 @@
+namespace Mirage.Graphics.Interfaces;
+
+/// <summary>
+/// Defines an object capable of producing graphical data for rendering.
+/// </summary>
+public interface IRenderable
+{
+    /// <summary>
+    /// Produces the graphical data required to render this object.
+    /// </summary>
+    /// <param name="context">
+    /// The context describing the current rendering frame.
+    /// </param>
+    /// <returns>
+    /// The backend-agnostic graphical data produced by this object.
+    /// </returns>
+    RenderData Render(RenderContext context);
+}
