f2c43f037b511a246407d511cdc4656abf6e804c
Max <max@stonebogus.com>
2026-09-22 23:05:17 -0600

    feat: create RenderContext & RenderData classes

--- Engine/Graphics/RenderContext.cs
+++ Engine/Graphics/RenderContext.cs
@@ -1,3 +1,25 @@
+using Mirage.Graphics.Primitives;
+
 namespace Mirage.Graphics;
 
-public class RenderContext { }
+/// <summary>
+/// Describes the current rendering frame.
+/// </summary>
+/// <param name="deltaTime">
+/// The elapsed time since the previous frame, in seconds.
+/// </param>
+/// <param name="clearColor">
+/// The color used to clear the frame.
+/// </param>
+public sealed class RenderContext(double deltaTime, Color clearColor)
+{
+    /// <summary>
+    /// Gets the color used to clear the frame.
+    /// </summary>
+    public Color ClearColor { get; } = clearColor;
+
+    /// <summary>
+    /// Gets the elapsed time since the previous frame, in seconds.
+    /// </summary>
+    public double DeltaTime { get; } = deltaTime;
+}
--- Engine/Graphics/RenderData.cs
+++ Engine/Graphics/RenderData.cs
@@ -1,3 +1,25 @@
+using Mirage.Graphics.Commands;
+
 namespace Mirage.Graphics;
 
-public class RenderData { }
+/// <summary>
+/// Contains the rendering commands produced by a renderable object.
+/// </summary>
+public sealed class RenderData
+{
+    private readonly List<RenderCommand> _commands = [];
+
+    /// <summary>
+    /// Gets the commands in their execution order.
+    /// </summary>
+    public IReadOnlyList<RenderCommand> Commands => _commands;
+
+    /// <summary>
+    /// Adds a rendering command.
+    /// </summary>
+    public void Add(RenderCommand command)
+    {
+        ArgumentNullException.ThrowIfNull(command);
+        _commands.Add(command);
+    }
+}
